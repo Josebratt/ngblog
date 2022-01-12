7b8592c78e3937515e7a5ee27838854280391c44
Josebratt <josebratt@gmail.com>
2022-01-12 19:04:09 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { CoreModule } from './core/core.module';
 
@@ -12,7 +11,6 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     MaterialModule, 
     CoreModule
   ],
