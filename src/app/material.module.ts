9da1b448a88aa823853e02bc80e30607918dbe33
Josebratt <josebratt@gmail.com>
2022-01-12 19:04:15 -0400

    Update material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
