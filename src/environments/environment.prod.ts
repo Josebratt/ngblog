54b8ef98298cec702d79ceed4c9a972c4b6c2cac
Josebratt <josebratt@gmail.com>
2022-01-13 11:42:13 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'mat-blog',
+    appId: '1:638804204465:web:d8716dba12df4cd3263b90',
+    storageBucket: 'mat-blog.appspot.com',
+    apiKey: 'AIzaSyDDbQN9YhF5gvbuUjznQU4rqPqgWVQ2wSM',
+    authDomain: 'mat-blog.firebaseapp.com',
+    messagingSenderId: '638804204465',
+  },
   production: true
 };
