d23528f3d028c722fb12470296938fcc1e39907e
Josebratt <josebratt@gmail.com>
2022-01-13 11:42:29 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mat-blog',
+    appId: '1:638804204465:web:d8716dba12df4cd3263b90',
+    storageBucket: 'mat-blog.appspot.com',
+    apiKey: 'AIzaSyDDbQN9YhF5gvbuUjznQU4rqPqgWVQ2wSM',
+    authDomain: 'mat-blog.firebaseapp.com',
+    messagingSenderId: '638804204465',
+  },
   production: false
 };
 
